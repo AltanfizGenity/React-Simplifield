026f1f41f6a3e1a582d290fd42976630c09348e4
Altanfiz <altanfizgenity@gmail.com>
2024-01-17 13:56:23 +0700

    raw navbar

--- src/apps/TodoApp/components/Navbar.jsx
+++ src/apps/TodoApp/components/Navbar.jsx
@@ -1,17 +1,10 @@
+import MenuBtn from "./MenuBtn";
+
 function Navbar() {
   return (
-    <nav className="fixed w-full h-[32px] p-8">
-      <div className="flex w-full justify-between content-center">
-        <div className="title font-bold text-xl">Todo List</div>
-        <ul className="nav-btn flex justify-center content-center gap-5">
-          <li>
-            <button className="icon-btn">menu btn</button>
-          </li>
-          <li>
-            <button className="icon-btn">list btn</button>
-          </li>
-        </ul>
-      </div>
+    <nav className="fixed w-full p-10 flex justify-between items-center bg-transparent">
+      <div className="title font-bold text-xl">Todo List</div>
+      <MenuBtn />
     </nav>
   );
 }
