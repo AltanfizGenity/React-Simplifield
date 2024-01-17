e3fc3321b2fe47af6261e0e59573cff4d89ddbe2
Altanfiz <altanfizgenity@gmail.com>
2024-01-17 14:41:40 +0700

    add gradient to main title

--- src/apps/TodoApp/components/Navbar.jsx
+++ src/apps/TodoApp/components/Navbar.jsx
@@ -4,7 +4,9 @@ import SearchBtn from "./SearchBtn";
 function Navbar() {
   return (
     <nav className="fixed w-full p-10 flex justify-between items-center bg-transparent">
-      <div className="title font-bold text-xl">Todo List</div>
+      <div className="title font-bold text-md primary-gradient-text">
+        Todospace
+      </div>
       <ul className="flex justify-center items-center gap-5">
         <SearchBtn />
         <MenuBtn />
